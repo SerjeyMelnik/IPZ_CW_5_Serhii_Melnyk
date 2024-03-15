30c9c92dcee6db8960ed1ca5a63ba9f16585ca02
Serhii Melnyk <serjeymelnik@gmail.com>
2024-03-15 13:46:48 +0200

    init

---
+++ app/src/main/java/com/example/ipz_cw_5/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ipz_cw_5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ipz_cw_5.ui.theme.IPZ_CW_5_Serhii_MelnykTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IPZ_CW_5_Serhii_MelnykTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IPZ_CW_5_Serhii_MelnykTheme {
+        Greeting("Android")
+    }
+}
